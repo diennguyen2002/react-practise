20e14a582e25c22e294fb6f915f3ea3ff305d392
diennguyen2002 <diennguyen2002@gmail.com>
2025-10-02 08:56:11 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h2>Demo Counter</h2>
+        <Counter />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(c=>c+1)}>+</button>
+            <button onClick={()=>setCount(c=>c-1)}>-</button>
+        </div>
+    );
+}
